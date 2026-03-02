281d468ee2bca97ddb9afebf318aea60b1b9238a
Amiel <amiel.voldsund@gmail.com>
2026-03-02 11:31:01 +0000

    commit

--- netlify/functions/get-lora-data.js
+++ netlify/functions/get-lora-data.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 exports.handler = async function(event, context) {
   // 1. CHANGE THESE TO YOUR OWN TTN INFO
   const TTN_REGION = "eu1"; // e.g., eu1, nam1
-  const APP_ID = "your-application-id"; 
+  const APP_ID = "sender"; 
   const API_KEY = process.env.TTN_API_KEY; // This pulled from Netlify settings
 
   const url = `https://${TTN_REGION}.cloud.thethings.network/api/v3/as/applications/${APP_ID}/packages/storage/uplink_message?limit=1&order=-received_at`;
