33e34f359f03754f7cb4e4f4a72e22dd88ac2507
Amiel <amiel.voldsund@gmail.com>
2026-03-02 12:34:38 +0000

    commit

--- Brain.js
+++ Brain.js
@@ -7,4 +7,12 @@ async function start() {
     console.log("Data received:", json);
 }
 
+if (result.messages && result.messages.length > 5) { // Check if we actually have text
+    const lines = result.messages.trim().split('\n');
+    const lastLine = JSON.parse(lines[lines.length - 1]);
+    console.log("Found newest data:", lastLine);
+} else {
+    console.log("TTN has no saved messages yet. Send data from the Arduino!");
+}
+
 start();
